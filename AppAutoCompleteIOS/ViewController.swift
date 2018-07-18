3c5f0c6ec8bd416fcb8a2f5cb54b39e235b469a6
AhmedALRUSAINI <ahmad@alamat.tech>
2018-07-18 12:25:44 +0300

    v1

--- AppAutoCompleteIOS/ViewController.swift
+++ AppAutoCompleteIOS/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
-
+import Alamofire
+import SwiftyJSON
+import SearchTextField
+/*
+   repo : https://github.com/ahmedalrusaini/SearchTextField.git
+   just add pod 'SearchTextField' in Project target in PODFILE $ commande in dir project pod install
+   in readme The Way of usage of this lib .
+*/
 class ViewController: UIViewController {
-
+    @IBOutlet weak var txtAutoComplete: SearchTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        getCities()
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    func getCities() {
+        var Cities:[String] = [String]()
+        Alamofire
+            .request("https://apina.address.gov.sa/NationalAddress/v3.1/lookup/cities?format=JSON&regionid=-1&language=E&api_key=03276a0c4d6f46719efba071a0c3b8be")
+            .responseJSON
+            { (responseData) -> Void in
+                if((responseData.result.value) != nil) {
+                    let swiftyJsonVar1 = JSON(responseData.result.value!)
+                    let strjson = swiftyJsonVar1.rawString()!.data(using: String.Encoding.utf8)!
+                    let swiftyJsonVar = JSON(strjson)
+                    if let resData = swiftyJsonVar["Cities"].arrayObject {
+                        for a in resData {
+                            let v = a as! [String:Any]
+                            let City = v["Name"]!
+                            Cities.append(City as! String)
+                        }
+                        self.txtAutoComplete.filterStrings(Cities) // put data from array to Component AutoComplete
+                        self.txtAutoComplete.startVisible = true
+                        self.txtAutoComplete.maxNumberOfResults = 5
+                    }
+                }
+        }
+        }
 }
-
---
+++ Pods/Target Support Files/Pods-AppAutoCompleteIOS/Pods-AppAutoCompleteIOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppAutoCompleteIOS : NSObject
+@end
+@implementation PodsDummy_Pods_AppAutoCompleteIOS
+@end
